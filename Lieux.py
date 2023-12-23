0a0c198bde2244b2db6f541c0ed9a21163b765e2
saraht2000 <151960968+saraht2000@users.noreply.github.com>
2023-12-23 12:47:09 +0000

    bon calendrier

--- Lieux.py
+++ Lieux.py
@@ -1,4 +1,22 @@
-lieux = ['Stade Tour Eiffel','Arena Champ-de-Mars', 'Grand Palais', 'Invalides', 'Hôtel de Ville', 'Pont Alexandre III', 'Pont Iéna', 'La Concorde', 'Stade Rolland-Garros', 'Arena Paris Sud', 'Arena Bercy', 'Arena Porte de la Chapelle', 'Paris la Défense Arena', 'Centre aquatique', 'Stade de France', 'Site escalade du Bourget', 'Arena Paris Nord']
+lieux = ['Stade Tour Eiffel','Arena Champ-de-Mars', 'Grand Palais', 'Invalides', 'Pont Alexandre III', 'Trocadéro', 'La Concorde', 'Stade Roland-Garros', 'Arena Paris Sud', 'Arena Bercy', 'Arena Porte de la Chapelle', 'Paris la Défense Arena', 'Centre aquatique', 'Stade de France', 'Site escalade du Bourget', 'Arena Paris Nord']
 
 
-print(vecteur)
+lieux = [
+'stade-tour-eiffel',
+'arena-champ-de-mars',
+'grand-palais',
+'invalides',
+'hotel-de-ville',
+'pont-alexandre-iii',
+'trocadero', 
+'la-concorde',
+'stade-roland-garros',
+'arena-paris-sud', 
+'arena-bercy', 
+'arena-la-chapelle', 
+'paris-la-defense-arena', 
+'centre-aquatique', 
+'stade-de-france',
+'site-escalade-bourget',
+'arena-paris-nord'
+]
